dbe3e7188883027bf1ebd9252d2b00934e50eea0
jpdrsanchez <joaopedrosanchez302@gmail.com>
2023-02-23 16:36:38 -0300

    feat(backend): create viewers repository interface

---
+++ backend/src/domain/repositories/viewers.repository.ts
@@ -0,0 +1,9 @@
+import { Viewer } from '@domain/entities/viewer.entity'
+
+export abstract class ViewerRepository {
+  abstract create(viewer: Viewer): Promise<void>
+  abstract update(viewer: Viewer): Promise<void>
+  abstract findById(viewerId: string): Promise<Viewer>
+  abstract findByEmail(viewerEmail: string): Promise<Viewer>
+  abstract findAll(): Promise<Viewer[]>
+}
